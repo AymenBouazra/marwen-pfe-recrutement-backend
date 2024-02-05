64166a631e295f0cab1b3942b5dfa5b43dd31b28
Aymen <aymenbouazra994@gmail.com>
2024-02-05 13:23:29 +0100

    cors

--- database/connect.js
+++ database/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const developementURI = process.env.DEV_URI
 const testURI = process.env.DB_URI
 mongoose.set('strictQuery', false);
-mongoose.connect(testURI).then(success => {
+mongoose.connect(developementURI).then(success => {
     console.log('Successfuly connected to database');
 }).catch(error => {
     console.log('error connecting to database');
